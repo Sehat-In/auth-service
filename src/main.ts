3b25fe9af9d4b37b262f12e5c9d6e4df8bc4ceac
TehGaa <syosuasaputra1010@gmail.com>
2024-05-07 17:06:17 +0700

    trying to fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(process.env.PORT);
 }
 bootstrap();
