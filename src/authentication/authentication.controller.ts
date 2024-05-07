4df794d1aa89de20934b4f380018331329fa0792
TehGaa <syosuasaputra1010@gmail.com>
2024-05-07 16:53:26 +0700

    trying to fix cookie problem

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,35 +1,50 @@
-import { Body, Controller, Get, Post, Redirect, Req, Res, UseGuards, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Redirect,
+  Req,
+  Res,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthGuard } from '@nestjs/passport';
 import { UserRequest } from './dto';
 
 @Controller('api/v1/auth')
 export class AuthenticationController {
-    constructor(private authService: AuthenticationService) {}
-    
-    @Get('login-google/callback')
-    @UseGuards(AuthGuard('google'))
-    async loginGoogleRedirect(@Req() request, @Res() response) {
-        await this.authService.callbackGoogle(request, response);
-    }
+  constructor(private authService: AuthenticationService) {}
 
-    @Get('login-google')
-    @UseGuards(AuthGuard('google'))
-    async loginGoogle() {}
+  @Get('login-google/callback')
+  @UseGuards(AuthGuard('google'))
+  async loginGoogleRedirect(@Req() request, @Res() response) {
+    await this.authService.callbackGoogle(request, response);
+  }
 
-    @Post('login')
-    async login(@Body() request: UserRequest) {
-        return this.authService.login(request);
-    }
+  @Get('login-google')
+  @UseGuards(AuthGuard('google'))
+  async loginGoogle() {}
 
-    @Post('register')
-    async register(@Body() request: UserRequest) {
-        return this.authService.register(request);
-    }
+  @Post('login')
+  async login(@Body() request: UserRequest) {
+    return this.authService.login(request);
+  }
 
-    @Get('refresh')
-    @UseGuards(AuthGuard('jwt'))
-    async refresh(@Req() request) {
-        return this.authService.refresh(request.user);
-    }
+  @Post('register')
+  async register(@Body() request: UserRequest) {
+    return this.authService.register(request);
+  }
+
+  @Get('refresh')
+  @UseGuards(AuthGuard('jwt'))
+  async refresh(@Req() request) {
+    return this.authService.refresh(request.user);
+  }
+
+  @Get('dummy')
+  async dummy(@Req() request) {
+    return new Response('hello auth!');
+  }
 }
